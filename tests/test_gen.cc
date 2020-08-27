cdce830c474472a99f89a4b1a18ecfce8e77cf10
Jose Felix Rivas Carrasco <josefelixrc@outlook.com>
2020-08-27 09:35:57 -0400

    test: Change the framework CppUnit for Googletest

--- tests/test_gen.cc
+++ tests/test_gen.cc
@@ -1,92 +1,50 @@
-#include <iostream>
-#include <string>
-#include <list>
-#include <cppunit/TestCase.h>
-#include <cppunit/TestFixture.h>
-#include <cppunit/ui/text/TextTestRunner.h>
-#include <cppunit/extensions/HelperMacros.h>
-#include <cppunit/extensions/TestFactoryRegistry.h>
-#include <cppunit/TestResult.h>
-#include <cppunit/TestResultCollector.h>
-#include <cppunit/TestRunner.h>
-#include <cppunit/BriefTestProgressListener.h>
-#include <cppunit/CompilerOutputter.h>
-#include <cppunit/XmlOutputter.h>
-#include <netinet/in.h>
 
+#include <iostream>
+#include "gtest/gtest.h"
 #include "cpw_notifications.h"
 
-//-----------------------------------------------------------------------------
-
-class TestGen : public CppUnit::TestFixture
+class TestGen : public ::testing::Test
 {
-	CPPUNIT_TEST_SUITE(TestGen);
-	CPPUNIT_TEST(testViewNotification_);
-	CPPUNIT_TEST(testViewVersionSoftware_);
-	CPPUNIT_TEST_SUITE_END();
-
-	public:
-		TestGen();
-		void setUp(void);
-		void tearDown(void);
-
 	protected:
-		void testViewNotification_(void);
-		void testViewVersionSoftware_(void);
+		void SetUp() override;
+		void TearDown() override;
 
-	private:
-		CPWNotifications *mTestObj;
+		CPWNotifications *TestObj_;
 };
 
 //-----------------------------------------------------------------------------
 
-TestGen::TestGen(){}
-
-void TestGen::testViewNotification_(void)
+void TestGen::SetUp()
 {
-	mTestObj->ReceiveData_((char*)"app.id.com", (char*)"nombre", (char*)"cuerpo", (char*)"battery-low");
-	CPPUNIT_ASSERT(true == mTestObj->PrepareNotification_());
-	mTestObj->AddThemedIcon_();
-	mTestObj->ShowNotification_();
+    TestObj_ = new CPWNotifications();
 }
 
-void TestGen::testViewVersionSoftware_(void)
+void TestGen::TearDown()
 {
-	CPPUNIT_ASSERT(0 == cpw_notifications_VERSION_MAJOR);
-	CPPUNIT_ASSERT(0 == cpw_notifications_VERSION_MINOR);
-	CPPUNIT_ASSERT(1 == cpw_notifications_VERSION_PATCH);
-}
-
-void TestGen::setUp(void)
-{
-    mTestObj = new CPWNotifications();
-}
-
-void TestGen::tearDown(void)
-{
-    delete mTestObj;
+    delete TestObj_;
 }
 
 //-----------------------------------------------------------------------------
 
-CPPUNIT_TEST_SUITE_REGISTRATION(TestGen);
+TEST_F(TestGen, ViewVersionSoftware)
+{
+    ASSERT_EQ(0, cpw_notifications_VERSION_MAJOR);
+    ASSERT_EQ(0, cpw_notifications_VERSION_MINOR);
+    ASSERT_EQ(1, cpw_notifications_VERSION_PATCH);
+}
+
+TEST_F(TestGen, ViewNotification)
+{
+	TestObj_->ReceiveData_((char*)"app.id.com", (char*)"nombre", (char*)"cuerpo", (char*)"battery-low");
+	ASSERT_EQ(true, TestObj_->PrepareNotification_());
+	TestObj_->AddThemedIcon_();
+	TestObj_->ShowNotification_();
+}
+
+//-----------------------------------------------------------------------------
 
 int main(int argc, char* argv[])
 {
-    CPPUNIT_NS::TestResult testresult;
-
-    CPPUNIT_NS::TestResultCollector collectedresults;
-    testresult.addListener (&collectedresults);
-
-    CPPUNIT_NS::BriefTestProgressListener progress;
-    testresult.addListener (&progress);
-
-    CPPUNIT_NS::TestRunner testrunner;
-    testrunner.addTest (CPPUNIT_NS::TestFactoryRegistry::getRegistry().makeTest());
-    testrunner.run(testresult);
-
-    CPPUNIT_NS::CompilerOutputter compileroutputter(&collectedresults, std::cerr);
-    compileroutputter.write ();
-
-    return collectedresults.wasSuccessful() ? 0 : 1;
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
